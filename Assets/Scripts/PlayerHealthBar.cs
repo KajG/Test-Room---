fdf162587740244ff78e739c587eab532b8ec6e2
Kaj <kaaiman99@hotmail.com>
2017-02-03 20:14:53 +0100

    Changes

--- Assets/Scripts/PlayerHealthBar.cs
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -21,14 +21,6 @@ public class PlayerHealthBar : MonoBehaviour {
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag == "bullet") {
-			print ("asds");
-			bullet = other.gameObject.GetComponent<BulletMovement> ();
-			_health -= bullet.getDamage;
-			Destroy (other.gameObject);
-			Instantiate (damageText, new Vector2(Random.Range(transform.position.x, transform.position.x - 1f), Random.Range(transform.position.y + 1, transform.position.y + 2)), Quaternion.identity);
-			damageText.text = ("-" + bullet.getDamage);
-			healthbar.value = _health;
-		}
+
 	}
 }
---
+++ Assets/Scripts/UIBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBehaviour : MonoBehaviour {
+
+	[SerializeField] private Image _menu;
+	[SerializeField] private Button _resume;
+	[SerializeField] private Button _quit;
+	void Start(){
+		_resume.enabled = false;
+		_quit.enabled = false;
+		_menu.enabled = false;
+	}
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Time.timeScale = 0;
+			_resume.enabled = true;
+			_quit.enabled = true;
+			_menu.enabled = true;
+			if (_menu.enabled) {
+				Time.timeScale = 1;
+				_resume.enabled = false;
+				_quit.enabled = false;
+				_menu.enabled = false;
+			}
+		}
+	}
+	public void Quit(){
+		Application.Quit ();
+	}
+	public void Resume(){
+		Time.timeScale = 1;
+		_resume.enabled = false;
+		_quit.enabled = false;
+		_menu.enabled = false;
+	}
+}
